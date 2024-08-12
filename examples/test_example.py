b0234ed1bb1a1123e67b9904aedb4dad131e4c2b
Danny Jeng <djeng@nference.net>
2024-08-12 16:23:29 -0400

    linted test_example

--- examples/test_example.py
+++ examples/test_example.py
@@ -1,10 +1,17 @@
+'''
+Example file illustrating use of the InlineAlgoQueueProcessor base class.
+'''
 from inline_algorithm.inline_algo_queue_processor import InlineAlgoQueueProcessor
 
 class TestChild(InlineAlgoQueueProcessor):
-    def __init__(self, port, host, docker_mode=True):
-        super().__init__(port, host, docker_mode)
-    
+    '''
+    Example child class of InlineAlgoQueueProcessor.
+    '''
     def process(self, message):
+        '''
+        Method for processing incoming message from the scanner.
+        '''
+        print(f'Incoming message: {message}')
         return [[1, 2, 1, 'test_output']]
 
 if __name__== '__main__':
