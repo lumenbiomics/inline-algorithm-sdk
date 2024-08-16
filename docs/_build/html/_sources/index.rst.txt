e9652b5d71ccd7ce30cd43173e613b25fb4a7525
Danny Jeng <djeng@nference.net>
2024-08-16 16:00:32 -0400

    Initial commit of sphinx documentation

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. Pramana Inline Algorithm SDK documentation master file, created by
+   sphinx-quickstart on Fri Aug 16 13:54:16 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Pramana Inline Algorithm SDK documentation
+==========================================
+
+This package is a simple way to prepare your inline algorithms for running inline with Pramana's
+scanner. It will help to take the complexity out of creating your own API server and ensure that all
+required endpoints are called correctly.
+
+If you prefer to use a different programming language, or just want more control over your code,
+feel free to refer to this `API documentation
+<https://developers.pramana.ai/inline-algorithms/documentation>`_  to see all the required API
+endpoints.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   usage
+   example
---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pramana Inline Algorithm SDK'
+copyright = '2024, Sagar Potnis, Danny Jeng'
+author = 'Sagar Potnis, Danny Jeng'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
