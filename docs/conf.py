670e0af524482d9e260dec7d45a74a9a041c04e9
djeng-nference <119889980+djeng-nference@users.noreply.github.com>
2024-08-26 16:00:33 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Pramana Inline Algorithm SDK'
-copyright = '2024, Pramana'
+copyright = '2024 Pramana, Inc.'
 author = 'Sagar Potnis, Danny Jeng'
 
 # -- General configuration ---------------------------------------------------
