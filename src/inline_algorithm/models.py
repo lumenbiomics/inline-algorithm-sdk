d9ebfb2c686a20cb96a683e15d014ca9d1615aab
Danny Jeng <djeng@pramana.ai>
2025-01-23 17:34:56 +0000

    Merged in ISSUE-14661-fix-none-return (pull request #1)
    * If None is returned for detection array, default it to a list of lists.
    
    Approved-by: Falgun Dnyanendra Meshram

--- src/inline_algorithm/models.py
+++ src/inline_algorithm/models.py
@@ -21,7 +21,7 @@ under the License.
 Defining Pydantic models
 '''
 from typing import List, Union
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 class ScanStart(BaseModel):
     '''
@@ -79,6 +79,10 @@ class AoiResults(BaseModel):
     row_idx: int
     col_idx: int
     z_stack_to_preserve: bool | None = None
+
+    @validator("detection_array", pre=True, always=True)
+    def set_default_detection_array(cls, value):
+        return value if value is not None else [[]]
 
 class TileResults(BaseModel):
     '''
